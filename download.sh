31954b08fcc9b5db41724ef89f392391a54d5e47
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-03-19 23:56:32 -0700

    awk script to call script on each download percentage

---
+++ download.sh
@@ -0,0 +1,7 @@
+wget https://downloads.raspberrypi.org/NOOBS_lite_latest --progress=dot -q --show-progress 2>&1 | awk 'NF>2 && $(NF-2) ~ /%/{
+        cmd = "/home/brad/automate/beetle/set_percentage.sh " $(NF-2)
+	system(cmd); 
+        printf "\r %s",$(NF-2)} 
+	END{
+	    print "\r "	
+	}'
