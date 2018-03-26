548627d399574d73a8e8ecf493fb091da4e3ae6a
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-03-25 23:56:01 -0700

    -

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
+const path = require('path');
 const express = require('express');
+const formidable = require('formidable');
+const uuid = require('uuid');
+const tar = require('tar');
+const fs = require('fs-extra');
 
 const app = express();
 
@@ -29,5 +34,57 @@ app.post('/targets/:target', (req, res) => {
   }
   res.send('ok');
 });
+
+const getPackageNameFromTmpDirectory = tmpDirectory => new Promise((resolve, reject) => {
+  fs.readFile(path.resolve(tmpDirectory, 'manifest.json'), (err, result) => {
+    if (err){
+      reject(err);
+      return;
+    }
+    const info = JSON.parse(result);
+    const name = info.name;
+    resolve(name);
+  })
+});
+
+
+const processRawPackage = async (tmp_upload_file, tmpDirectory) => {
+  await fs.mkdirp(tmpDirectory);
+  await tar.x({
+    file: tmp_upload_file,
+    cwd: tmpDirectory,
+  });
+
+  const packageName = await getPackageNameFromTmpDirectory(tmpDirectory);
+
+  const deleteTmpUploadPromise  = new Promise((resolve, reject) => fs.unlink(tmp_upload_file, err => {
+    if (err){
+      reject(err);
+      return;
+    }
+    resolve();
+  }));
+
+  await deleteTmpUploadPromise;
+  await fs.remove(tmpDirectory);
+
+};
+
+app.post('/package', (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.on('fileBegin', (name, file) => {
+    file.path = path.resolve('./package_tmp', uuid());
+  });
+
+  form.parse(req, async (err, fields, files) => {
+    await Object.keys(files).map(async fileName => {
+      const filePath = files[fileName].path;
+      await processRawPackage(filePath, path.resolve('./package', path.basename(filePath)));
+    });
+    res.send('ok');
+  });
+});
+
+
 
 app.listen(3000);
